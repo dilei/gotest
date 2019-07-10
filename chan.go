287a234adc16a39642d8bbbf228c7494faf22bc6
dilei <dilei@dangdang.com>
2019-07-10 20:19:54 +0800

    update chan

--- chan.go
+++ chan.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 func main() {
 // 示例1。
@@ -23,26 +25,38 @@ _ = ch3
 */
 
 func main() {
+	// deadlock
+	// 对于非缓冲通道，情况要简单一些。无论是发送操作还是接收操作，一开始执行就会被阻塞
+	// 直到配对的操作也开始执行，才会继续传递， 也就是说只有收发双方对接上了，数据才会被传递
+	// c := make(chan int)
+	// c <- 1
+	// <-c
 
+	ch1 := make(chan int, 3)
+	ch1 <- 1
+	ch1 <- 2
+	ch1 <- 3
+	elem1 := <-ch1
+	fmt.Printf("first element: %v", elem1)
 
 	/*
-	c := make(chan int)
-	o := make(chan bool)
+		c := make(chan int)
+		o := make(chan bool)
 
-	// <- o // 1、如果直接读空通道，会直接报deadlock错误
+		// <- o // 1、如果直接读空通道，会直接报deadlock错误
 
-	go func() {
-		for {
-			select {
-			case v := <- c:
-				println(v)
-			case <- time.After(5 * time.Second):
-				println("timeout")
-				o <- true  // 相当于一个标记，通道可读，函数执行结束
-				break
+		go func() {
+			for {
+				select {
+				case v := <- c:
+					println(v)
+				case <- time.After(5 * time.Second):
+					println("timeout")
+					o <- true  // 相当于一个标记，通道可读，函数执行结束
+					break
+				}
 			}
-		}
-	}()
-	<- o  // 2、当上面有一个并发线程时，他的作用是阻塞函数直到通道可读，即使线程没有写通道
+		}()
+		<- o  // 2、当上面有一个并发线程时，他的作用是阻塞函数直到通道可读，即使线程没有写通道
 	*/
 }
